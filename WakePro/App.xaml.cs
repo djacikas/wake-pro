1a009f50598d4e1ef030bfc5ea1dcfc20153a605
djacikas <dovydas.jacikas@gmail.com>
2024-06-07 17:05:14 +0300

    Add initial MAUI app

---
+++ WakePro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WakePro
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         MainPage = new AppShell();
+      }
+   }
+}
---
+++ WakePro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WakePro
+{
+   public partial class AppShell : Shell
+   {
+      public AppShell()
+      {
+         InitializeComponent();
+      }
+   }
+}
